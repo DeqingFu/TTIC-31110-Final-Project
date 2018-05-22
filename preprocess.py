542de63c9860abde404a783c66bb73a2a02a7227
DeqingFu <deqing@uchicago.edu>
2018-05-22 01:39:21 -0500

    modify for cross platform

--- preprocess.py
+++ preprocess.py
@@ -15,22 +15,10 @@ features on the 100 hour Librispeech dataset.
 import os
 import glob
 import soundfile as sf
-#from python_speech_features import mfcc
 import numpy as np
 import tensorflow as tf
 from tqdm import tqdm
 
-
-'''
-def compute_mfcc(audio_data, sample_rate):
-
-    audio_data = audio_data - np.mean(audio_data)
-    audio_data = audio_data / np.max(audio_data)
-    mfcc_feat = mfcc(audio_data, sample_rate, winlen=0.025, winstep=0.01,
-                     numcep=13, nfilt=26, nfft=512, lowfreq=0, highfreq=None,
-                     preemph=0.97, ceplifter=22, appendEnergy=True)
-    return mfcc_feat
-'''
 
 def make_example(seq_len, spec_feat, labels):
     ''' Creates a SequenceExample for a single utterance.
@@ -57,10 +45,8 @@ def make_example(seq_len, spec_feat, labels):
 
     '''
     # Feature lists for the sequential features of the example
-    #feats_list = [tf.train.Feature(float_list=tf.train.FloatList(value=frame)) for frame in spec_feat]
     feats_list  = [tf.train.Feature(float_list=tf.train.FloatList(value=spec_feat))]
     feat_dict = {"feats": tf.train.FeatureList(feature=feats_list)}
-    #feat_dict = {"feats": feats_list}
     
     sequence_feats = tf.train.FeatureLists(feature_list=feat_dict)
 
@@ -95,19 +81,18 @@ def process_data(partition):
     transcripts = {}
     utt_len = {}  # Required for sorting the utterances based on length
     cnt = 0
-    for filename in glob.iglob(partition+'/*.wav.trn', recursive=True):
+    for filename in glob.iglob(os.path.abspath(partition+'/*.wav.trn'), recursive=True):
         with open(filename, 'r') as file:
             cnt += 1
-            file_dir = AUDIO_PATH + file.readline()[3:-5]
+            file_dir = os.path.abspath(AUDIO_PATH + file.readline()[3:-5])
             print("sub-processing file number", cnt, "in", os.path.basename(partition))
             features_name = file_dir + ".data"
             labels_name = file_dir + ".zh"
-            features = np.loadtxt(features_name, delimiter=",")
-            labels = np.loadtxt(labels_name, delimiter=",")
+            features = np.loadtxt(os.path.abspath(features_name), delimiter=",")
+            labels = np.loadtxt(os.path.abspath(labels_name), delimiter=",")
             feats[file_dir] = features
             utt_len[file_dir] = feats[file_dir].shape[0]
             transcripts[file_dir] = labels.tolist()
-            #print(transcripts[file_dir])
     return feats, transcripts, utt_len
 
 
@@ -117,8 +102,8 @@ def create_records():
     into integers, and generates sequence examples for each utterance.
     Multiple sequence records are then written into TFRecord files.
     """
-    for partition in sorted(glob.glob(AUDIO_PATH+'/*')):
-        if os.path.basename(partition) in ["data", "README.TXT", "lm_phone", "lm_word"]:
+    for partition in sorted(glob.glob(os.path.abspath(AUDIO_PATH+'/*'))):
+        if os.path.basename(partition) in ["data", "README.TXT", "lm_phone", "lm_word", "processed"]:
             continue
         print('Processing' + partition)
         feats, transcripts, utt_len = process_data(partition)
@@ -129,7 +114,7 @@ def create_records():
         min_t = int(utt_len[sorted_utts[0]]/100)
 
         # Create destination directory
-        write_dir = '../processed/' + partition.split('/')[-1]
+        write_dir = os.path.abspath(AUDIO_PATH + 'processed/' + partition.split('/')[-1])
         if tf.gfile.Exists(write_dir):
             tf.gfile.DeleteRecursively(write_dir)
         tf.gfile.MakeDirs(write_dir)
@@ -140,14 +125,12 @@ def create_records():
             count = {}
             print('Processing training files...')
             for i in range(min_t, max_t+1):
-                filename = os.path.join(write_dir, 'train' + '_' + str(i) +
-                                        '.tfrecords')
+                filename = os.path.abspath(os.path.join(write_dir, 'train' + '_' + str(i) + '.tfrecords'))
                 writer[i] = tf.python_io.TFRecordWriter(filename)
                 count[i] = 0
 
             for utt in tqdm(sorted_utts):
-                example = make_example(utt_len[utt], feats[utt].tolist(),
-                                       transcripts[utt])
+                example = make_example(utt_len[utt], feats[utt].tolist(), transcripts[utt])
                 index = int(utt_len[utt]/100)
                 writer[index].write(example)
                 count[index] += 1
@@ -159,12 +142,10 @@ def create_records():
             # Remove bins which have fewer than 20 utterances
             for i in range(min_t, max_t+1):
                 if count[i] < 20:
-                    os.remove(os.path.join(write_dir, 'train' +
-                                           '_' + str(i) + '.tfrecords'))
+                    os.remove(os.path.abspath(os.path.join(write_dir, 'train' + '_' + str(i) + '.tfrecords')))
         else:
             # Create single TFRecord for dev and test partition
-            filename = os.path.join(write_dir, os.path.basename(write_dir) +
-                                    '.tfrecords')
+            filename = os.path.abspath(os.path.join(write_dir, os.path.basename(write_dir) + '.tfrecords'))
             print('Creating', filename)
             record_writer = tf.python_io.TFRecordWriter(filename)
             for utt in sorted_utts:
